08e7d468d32e6f3e50671c15368cd17f1b095bad
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2022-07-23 17:05:20 +0530

    23 july

--- 1.py
+++ 1.py
@@ -1,4 +1,13 @@
-a=20
-b=30
-c=a+b
-print("Hi Python",c)
+
+String1 = 'Welcome to the python world'
+print("String with single quote")
+print(String1)
+
+String2 = 'I\'am a programmer'
+print("String with double quote")
+print(String2)
+print(type(String2))
+
+
+
+
