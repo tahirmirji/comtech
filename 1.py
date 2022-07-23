0b8d3cc8609b472c84b8afc9c3c1407a06fec517
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2022-07-23 17:41:27 +0530

    Lists

--- 1.py
+++ 1.py
@@ -1,12 +1,33 @@
 
-String1 = 'Welcome to the python world'
-print("String with single quote")
-print(String1)
+# String1 = 'Welcome to the python world'
+# print("String with single quote")
+# print(String1)
 
-String2 = 'I\'am a programmer'
-print("String with double quote")
-print(String2)
-print(type(String2))
+# String2 = 'I\'am a programmer'
+# print("String with double quote")
+# print(String2)
+# print(type(String2))
+
+# list1 = [1,"Hello",'Python',10.5,[20,30,[40,50]]]
+# print(list1)
+# list1[4][2][1]="We changed the value"
+# print(list1)
+
+
+
+list1 = [1,2,3]
+list2 = [4,5,6]
+print(list1)
+print(list2)
+list3 = list1 + list2
+print(list3)
+list3.append(7)
+print(list3)
+
+list3.clear()
+print(list3)
+
+
 
 
 
