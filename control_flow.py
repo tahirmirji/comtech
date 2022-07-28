1fe0efe0c5179ede61009f0bc96d561a0037eeea
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2022-07-28 18:52:53 +0530

    28th july

--- control_flow.py
+++ control_flow.py
@@ -13,7 +13,106 @@
 #     print("last else c=",c," is big")
 
 
-lst =["Python","Hi","Hello",4,5]
-for i in range(1,5):
-    print(lst[i], end=",")
+# lst =["Python","Hi","Hello",4,5]
+# for i in range(1,5):
+#     print(lst[i], end=",")
     
+
+
+
+
+# a= 5
+# b =2
+
+# r=a+b
+# print(r," is the sum")
+# r=a-b
+# print(r," is the substraction")
+# r=a*b
+# print(r," is the mult")
+# r=a/b
+# print(r," is the div")
+# r=a//b
+# print(r," is the floor value after div")
+# r=a**b
+# print(r," is the a to the powers of b ")
+# r=a % b 
+# print(r," is the a modulo b remainder ")
+
+# a= 13
+# b=33
+# print(a>b," Greater")
+# print(a<b," Less than")
+# print(a>=b, " greater or equal")
+# print(a<=b," less than or equal to")
+# print(a!=b," Not equal to")
+# print(a==b," Equality")
+
+
+
+# 0-0
+# 1-1
+# 2-10
+# 3-11
+# 4-100
+# 5-101
+# 6-110
+# 7-111
+# 8-1000
+# 9-1001
+# 10-1010
+# 11-1011
+
+a=2
+b=4
+# 10 in binary 1010
+# 4 in binary  0100
+#   1110
+        
+                    
+                  
+#  ans         
+
+# print(a & b)
+# print(a | b)
+# print( ~ a )
+# print(a^b)    
+# print(b<<2)
+#      0100
+
+
+
+# a=2
+# b=3
+# c=a
+
+
+# print(a is not b)
+# print(id(a))
+# print(id(b))
+
+
+
+
+
+x=11
+y=20
+list =[10,20,30,40,50]
+
+if (x in list):
+    print(x," is present")
+else:
+    print(x," is not  present")
+
+
+
+
+
+
+
+
+
+
+
+
+
